14b3d42bafef802432681aa3ccf49b68b39072bc
vrb9999 <vbiradar9999@gmail.com>
2022-05-31 23:43:57 +0530

    UC-10 Day-23 Get count of person in city or state

---
+++ GetPersonCountByCityOrState/GetPersonCountByCityOrState/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace GetPersonCountByCityOrState
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO ADDRESS BOOK PROGRAM");
+            MainMenuOperation();
+        }
+        public static void MainMenuOperation()
+        {
+            AddressBookList addressBookList = new AddressBookList();
+            bool flag1 = true;
+            while (flag1)
+            {
+                string currentAddressBookName = "";
+                Console.WriteLine("\nEnter:\n1-To add a new address Book\n2-Print all address book\n3-To access an existing address book" +
+                "\n4-To search person in a state or city across multiple address books\n5-View all persons of a city or state\n6-To get count of contacts present at a city or state\n7-To exit");
+                int options1 = Convert.ToInt32(Console.ReadLine());
+                switch (options1)
+                {
+                    case 1:
+                        addressBookList.AddNewAddressBook();
+                        break;
+                    case 2:
+                        addressBookList.PrintAllAddressBookNames();
+                        break;
+                    case 3:
+                        currentAddressBookName = addressBookList.ExistingAddressBook();
+                        break;
+                    case 4:
+                        addressBookList.SearchPersonByCityOrState();
+                        break;
+                    case 5:
+                        AddressBook.ViewPeopleByCityOrState();
+                        break;
+                    case 6:
+                        addressBookList.GetCountByCityOrState();
+                        break;
+                    case 7:
+                        flag1 = false;
+                        break;
+                }
+                if (currentAddressBookName != "")
+                {
+                    bool flag2 = true;
+                    while (flag2)
+                    {
+                        Console.WriteLine("\nCurrent address book:" + currentAddressBookName);
+                        Console.WriteLine("Enter:\n1-To add a new contact\n2-To edit an existing contact\n3-To search for an existing contact\n4-To delete a contact\n5-To display all contacts in the address book\n6-To return to main menu");
+                        int options2 = Convert.ToInt32(Console.ReadLine());
+                        switch (options2)
+                        {
+                            case 1:
+                                addressBookList.addressBookListDictionary[currentAddressBookName].AddNewContact();
+                                break;
+                            case 2:
+                                addressBookList.addressBookListDictionary[currentAddressBookName].EditContact();
+                                break;
+                            case 3:
+                                addressBookList.addressBookListDictionary[currentAddressBookName].SearchContactByName();
+                                break;
+                            case 4:
+                                addressBookList.addressBookListDictionary[currentAddressBookName].DeleteContact();
+                                break;
+                            case 5:
+                                addressBookList.addressBookListDictionary[currentAddressBookName].ViewAllContacts();
+                                break;
+                            case 6:
+                                flag2 = false;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
