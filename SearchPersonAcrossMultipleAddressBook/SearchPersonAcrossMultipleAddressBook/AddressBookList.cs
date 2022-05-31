f9089f2f6a48c1446eecc0c21cff9ee18c7261bd
vrb9999 <vbiradar9999@gmail.com>
2022-05-31 23:40:07 +0530

    UC-8 Day-23 Search person across multiple address book

---
+++ SearchPersonAcrossMultipleAddressBook/SearchPersonAcrossMultipleAddressBook/AddressBookList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchPersonAcrossMultipleAddressBook
+{
+    class AddressBookList
+    {
+        public Dictionary<string, AddressBook> addressBookListDictionary = new Dictionary<string, AddressBook>();
+        //ADDS A NEW ADDRESS BOOK WITH UNIQUE NAME TO THE SYSTEM
+        public void AddNewAddressBook()
+        {
+            Console.WriteLine("Enter the name of address book:");
+            string addressBookName = Console.ReadLine();
+            AddressBook addressBook = new AddressBook(addressBookName);
+            // HANDLES THE EXCEPTION WHEN THE USER TRIES TO ENTER DUPLICATE ADDRESS BOOK NAME
+            try
+            {
+                addressBookListDictionary.Add(addressBookName, addressBook);
+                Console.WriteLine("\nAddress Book " + addressBookName + " added successfully");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Address Book {0} already exists, please access it or add a new address book with different name!", addressBookName);
+            }
+        }
+        /// CHECKS FOR AN EXISTING ADDRESS BOOK IN THE SYSTEM
+        public string ExistingAddressBook()
+        {
+            Console.WriteLine("Enter the Address Book name");
+            string addressBookName = Console.ReadLine();
+            string result = "";
+            bool flag = true;
+            foreach (var kvp in addressBookListDictionary)
+            {
+                if (kvp.Key == addressBookName)
+                {
+                    result = addressBookName;
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag == true)
+            {
+                Console.WriteLine("No Address Book with name " + addressBookName + " found\nPlease create a new address book");
+            }
+            return result;
+        }
+        public void ListOfAddressBook()
+        {
+            Console.WriteLine("List of Address Book");
+            foreach (KeyValuePair<string, AddressBook> listOfAddressBook in addressBookListDictionary)
+            {
+                Console.WriteLine(listOfAddressBook.Key);
+            }
+        }
+        public void SearchPersonInCityOrState(string firstName, string lastName)
+        {
+            foreach (var addressBookEntry in addressBookListDictionary)
+            {
+                List<Contact> PersonInCitiesOrStates = addressBookEntry.Value.contactList.FindAll(i => (i.firstName == firstName) && (i.lastName == lastName));
+                foreach (Contact person in PersonInCitiesOrStates)
+                {
+                    Console.WriteLine($" {person.firstName} {person.lastName} is in {person.city} {person.state}");
+                }
+            }
+        }
+    }
+}
---
+++ SearchPersonAcrossMultipleAddressBook/SearchPersonAcrossMultipleAddressBook/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace SearchPersonAcrossMultipleAddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO ADDRESS BOOK PROGRAM");
+            MainMenuOperation();
+        }
+        public static void MainMenuOperation()
+        {
+            AddressBookList addressBookList = new AddressBookList();
+            bool flag1 = true;
+            while (flag1)
+            {
+                string currentAddressBookName = "";
+                Console.WriteLine("\nEnter:\n1-To add a new address Book\n2-To access list of address book\n3-To access an existing address book\n4-Search person in city or state\n5-To exit");
+                int options1 = Convert.ToInt32(Console.ReadLine());
+                switch (options1)
+                {
+                    case 1:
+                        addressBookList.AddNewAddressBook();
+                        break;
+                    case 2:
+                        addressBookList.ListOfAddressBook();
+                        break;
+                    case 3:
+                        currentAddressBookName = addressBookList.ExistingAddressBook();
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter First Name");
+                        string firstName = Console.ReadLine();
+                        Console.WriteLine("Enter Last Name");
+                        string lastName = Console.ReadLine();
+                        addressBookList.SearchPersonInCityOrState(firstName, lastName);
+                        break;
+                    case 5:
+                        flag1 = false;
+                        break;
+                }
+                if (currentAddressBookName != "")
+                {
+                    bool flag2 = true;
+                    while (flag2)
+                    {
+                        Console.WriteLine("\nCurrent address book:" + currentAddressBookName);
+                        Console.WriteLine("Enter:\n1-To add a new contact\n2-To edit an existing contact\n3-To search for an existing contact\n4-To delete a contact\n5-To display all contacts in the address book\n6-To return to main menu");
+                        int options2 = Convert.ToInt32(Console.ReadLine());
+                        switch (options2)
+                        {
+                            case 1:
+                                addressBookList.addressBookListDictionary[currentAddressBookName].AddNewContact();
+                                break;
+                            case 2:
+                                addressBookList.addressBookListDictionary[currentAddressBookName].EditContact();
+                                break;
+                            case 3:
+                                addressBookList.addressBookListDictionary[currentAddressBookName].SearchContactByName();
+                                break;
+                            case 4:
+                                addressBookList.addressBookListDictionary[currentAddressBookName].DeleteContact();
+                                break;
+                            case 5:
+                                addressBookList.addressBookListDictionary[currentAddressBookName].ViewAllContacts();
+                                break;
+                            case 6:
+                                flag2 = false;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
